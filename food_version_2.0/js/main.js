31c77ac9c47e38b99a76f2d0985971593fd9fe03
=AnjanandPoudel <=47265676+AnjanandPoudel@users.noreply.github.com>
2020-02-06 17:23:12 +0545

    hotel-profile

--- food_version_2.0/js/main.js
+++ food_version_2.0/js/main.js
@@ -17,36 +17,14 @@ const responsive={
         items:3
     },
     1200:{
-        items:4
+        items:3
     },
     1550:{
-        items:5
+        items:4
     }
 }
 
-const response={
-    0:{
-        items:1
-    },
-    320:{
-        items:1
-    },
-    580:{
-        items:2
-    },
-    900:{
-        items:3
-    },
-    960:{
-        items:3
-    },
-    1200:{
-        items:4
-    },
-    1550:{
-        items:5
-    }
-}
+
 
 $(document).ready(function(){
 
